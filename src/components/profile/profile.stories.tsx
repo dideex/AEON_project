f94d26e4f09469a716b66c2ba3612d9e4ebf36c0
dideex <wc34fun@mail.ru>
2019-08-02 18:25:42 +0800

    Added a bit photos

--- src/components/profile/profile.stories.tsx
+++ src/components/profile/profile.stories.tsx
@@ -2,90 +2,14 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { Grid, Container } from '@material-ui/core'
+import { grey } from '@material-ui/core/colors'
 // import { BrowserRouter as RouterProvider } from 'react-router-dom'
 
 import MyProfileCmp from './my-profile'
 import { ProfileWidget } from './widget'
 import { Provider as ContextProvider } from '../common'
-import { TMyInfo } from '../../types'
-import { grey } from '@material-ui/core/colors'
 import { CustomThemeProvider as ThemeProvider } from '../../service'
-import { users } from '../../mocks'
-
-const me: TMyInfo = {
-  id: 'x000001',
-  username: 'Tony@stark.com',
-  firstname: 'Tony',
-  lastname: 'Stark',
-  city: 'New-York',
-  gender: 'male',
-  patronymic: 'Stark',
-  avatar: '/image/avatar/me.jpg',
-  about: 'Genius, Billionaire, Playboy, Philanthropist',
-  age: 66,
-  registered: '1547395200000',
-  isOnline: true,
-  birthDate: {
-    month: 2,
-    year: 1963,
-    day: 22,
-  },
-  statistic: {
-    posts: 13,
-    likes: 68,
-  },
-  photos: [
-    {
-      id: '0000',
-      title: 'Big insect in the city, call the police',
-      date: '1564713854069',
-      url: '/image/gallery/me_0.jpg',
-      likes: [users.batman],
-    },
-    {
-      id: '0001',
-      title: 'Istambul <3',
-      date: '1564213954069',
-      url: '/image/gallery/me_1.jpg',
-      likes: [],
-    },
-    {
-      id: '0002',
-      title: 'Hi-tech undewear',
-      date: '1564713954069',
-      url: '/image/gallery/me_2.jpg',
-      likes: [],
-    },
-    {
-      id: '0003',
-      title: 'Ironman plays with ironman',
-      date: '1564713959069',
-      url: '/image/gallery/me_3.jpg',
-      likes: [],
-    },
-    {
-      id: '0004',
-      title: 'Look at my red glasses',
-      date: '1564713954069',
-      url: '/image/gallery/me_4.jpg',
-      likes: [],
-    },
-    {
-      id: '0005',
-      title: 'Peace (=',
-      date: '1564213954069',
-      url: '/image/gallery/me_5.jpg',
-      likes: [],
-    },
-    {
-      id: '0006',
-      title: 'Sup?',
-      date: '1551113954069',
-      url: '/image/gallery/me_6.jpg',
-      likes: [],
-    },
-  ],
-}
+import { me } from '../../mocks'
 
 const MyProfile = (props: any) => (
   <ThemeProvider>
--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -12,4 +12,28 @@ export const batman: IUserPreview = {
   isOnline: true,
 }
 
-export default { batman }
+export const spinderman: IUserPreview = {
+  id: 'x0003',
+  username: 'pspidy@nyc.com',
+  firstname: 'Peter',
+  lastname: 'Parker',
+  avatar: '/image/avatar/spiderman.jpg',
+  city: 'New-York',
+  about: 'With great power comes great responsibility',
+  age: 59,
+  isOnline: true,
+}
+
+export const superman: IUserPreview = {
+  id: 'x0004',
+  username: 'superman@nyc.com',
+  firstname: 'Clark',
+  lastname: 'Kent',
+  avatar: '/image/avatar/superman.jpg',
+  city: 'New-York',
+  about: 'My sin? - salvation of the world',
+  age: 79,
+  isOnline: true,
+}
+
+export default { batman, spinderman, superman }
