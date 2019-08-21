3592834f51cd0067a37a9e6fc9dab14c0f71aa49
dideex <wc34fun@mail.ru>
2019-08-21 18:45:52 +0800

    Added posts

--- src/mocks/post.ts
+++ src/mocks/post.ts
@@ -12,5 +12,60 @@ export const post1: IPost = {
   author: stark,
   photo: '/image/post/post1.jpg',
   likes: [stark, users.ant],
+  date: minsFromNow(2),
+}
+
+export const post2: IPost = {
+  id: '2',
+  title: "'Marvel's Spider-Man' For PS4 Has Sold Over 13 Million Units Worldwide",
+  body: `As we near the first anniversary since the critically-acclaimed release of Marvel's Spider-Man, exclusively for PlayStation 4, Sony has revealed some exciting news about the game's reception!
+
+As of July 28, Marvel's Spider-Man has sold over 13.2 million units globally. This has not been the only records the game has broken; within the first few days on the market, the game became the PS4's fastest-selling game. Other milestones include becoming the best-selling Super Hero game in the United States, as well as being the highest sold game exclusively made for PS4.`,
+  views: 13,
+  author: users.spinderman,
+  photo: '/image/post/post2.jpg',
+  likes: [stark, users.ant, users.batman, users.blackWidow, users.drax],
+  date: minsFromNow(5),
+}
+
+export const post3: IPost = {
+  id: '3',
+  title: 'The 10 Most-Read Marvel News Stories of the Week: 8-16-19',
+  body: `Marvel.com has wrapped up another week of news for every type of fan out there, and we wanted to make sure you saw the top stories and features from the site the past few days!
+
+  Below, here's a quick refresher on everything you may have missed across the world of Marvel!`,
+  views: 15,
+  author: users.deadpool,
+  photo: '/image/post/post3.jpg',
+  likes: [
+    stark,
+    users.ant,
+    users.batman,
+    users.blackWidow,
+    users.drax,
+    users.deadpool,
+    users.daredevil,
+  ],
+  date: minsFromNow(11),
+}
+
+export const post4: IPost = {
+  id: '4',
+  title: 'Marvel Studios tops $5 billion at the global box office in 2019',
+  body: `Marvel Studios has reached yet another box office milestone. The studio has earned over $5.02 billion in global receipts in 2019, with just three films. Marvel topped its previous studio-best weeks ago, but there’s an even more impressive record the company has set for itself.`,
+  views: 19,
+  author: stark,
+  photo: '/image/post/post4.jpg',
+  likes: [
+    stark,
+    users.ant,
+    users.batman,
+    users.blackWidow,
+    users.drax,
+    users.deadpool,
+    users.daredevil,
+    users.marvel,
+    users.venom,
+  ],
   date: minsFromNow(15),
 }
