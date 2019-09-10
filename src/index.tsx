930cfe717c735bd11c2ae1e5aa2255214fa411f2
dideex <wc34fun@mail.ru>
2019-09-10 15:17:47 +0800

    Added hmr

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+if (module.hot && process.env.NODE_ENV !== 'production') {
+  module.hot.accept()
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
