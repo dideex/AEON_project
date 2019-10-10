d90e6773937cba1bd7206bfea691e24afce99a7b
dideex <wc34fun@mail.ru>
2019-10-10 09:01:50 +0300

    ➕register route 🛣

--- src/routes/root-router.tsx
+++ src/routes/root-router.tsx
@@ -2,11 +2,12 @@ import * as React from 'react'
 import { Switch, Route } from 'react-router'
 
 import { Landing } from '../layout'
-import { Auth } from '../components/auth'
+import { Auth, Register } from '../components/auth'
 
 export const RootRouter: React.FC = () => {
   return (
     <Switch>
+      <Route path="/register" component={Register} />
       <Route path="/auth" component={Auth} />
       <Route path="/" component={Landing} />
       <Route path="*" component={Landing} />
