3f6145a646b7970fb990970e804da3d793b56d83
dideex <wc34fun@mail.ru>
2019-10-11 08:29:54 +0300

    📦Added local storage service

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,3 +1,4 @@
 export { CustomThemeProvider } from './theme'
 export { CustomRouterProvider, useRouter } from './router-provider'
 export { CustomApolloProvider } from './apollo-provider'
+export * from './local-storage'
---
+++ src/service/local-storage.ts
@@ -0,0 +1,47 @@
+/**
+ * Получить объект из local storage
+ * @param {Название свойства} key
+ */
+export function read(key: string): string | null {
+  try {
+    return JSON.parse(localStorage.getItem(key) || '{}')
+  } catch {
+    return localStorage.getItem(key)
+  }
+}
+
+/**
+ * Записать объект в local storage
+ * @param {Название свойства} key
+ * @param {Данные} data
+ */
+export function write(key: string, data: object | string): void {
+  try {
+    if (typeof data === 'object') localStorage.setItem(key, JSON.stringify(data))
+    else localStorage.setItem(key, data)
+  } catch {}
+}
+
+/**
+ * Обновить объект в local storage
+ * @param {Название свойства} key
+ * @param {Данные} data
+ */
+export function update(key: string, data: object): void {
+  const info = read(key) || {}
+  if (info) {
+    try {
+      localStorage.setItem(key, JSON.stringify({ ...info, ...data }))
+    } catch {
+      console.error('Failed to update local storage: ', key)
+    }
+  }
+}
+
+/**
+ * Удалить объект из local storage
+ * @param {Название свойства} key
+ */
+export function remove(key: string): void {
+  localStorage.removeItem(key)
+}
