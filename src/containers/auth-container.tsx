7c90592ac699ef03683593445aa8581a5fa1057a
dideex <wc34fun@mail.ru>
2019-10-31 08:04:01 +0300

    🎢Fixed auth container

--- src/containers/auth-container.tsx
+++ src/containers/auth-container.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { useMutation } from '@apollo/react-hooks'
+
 import { UserAuthProvider } from '../components/common'
 import { IUserRequest } from '../types'
+import { Login } from '../graphql/mutations/login.graphql'
 
-// TODO: test it
 export const AuthContainer: React.FC = ({ children }) => {
   return (
     <AuthNetwork>
@@ -23,6 +25,9 @@ interface IAuthNetwork {
 
 const AuthNetwork: React.FC<IAuthNetwork> = ({ children, isLoading }) => {
   const loading = Boolean(isLoading)
-  const handleSubmit = () => {}
+  const handleSubmit = (sessionInput: IUserRequest) => {
+    const response = useMutation(Login, { variables: sessionInput })
+    console.log('TCL: handleSubmit -> response', response)
+  }
   return children({ handleSubmit, loading })
 }
